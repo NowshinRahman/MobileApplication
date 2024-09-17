aa317478bfa9b41c3d05d137d24c3f3d83c9d3e6
ReaganInsideJob <126184176+ReaganInsideJob@users.noreply.github.com>
2024-09-17 07:48:39 -0400

    added

---
+++ Week2/week2day2/hw2/src/component/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: 'var(--c-blue)', padding: '10px', color: 'white' }}>
+    </header>
+  );
+};
---
+++ Week2/week2day2/hw2/src/component/wrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Wrapper = ({ children }) => {
+  return (
+    <div style={{ backgroundColor: 'var(--c-blue)', padding: '10px' }}>
+      {children}
+    </div>
+  );
+};
+
+export default Wrapper;
